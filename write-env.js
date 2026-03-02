d71e6255baf63abc5ba076b025ba37ba7c4e41de
MikeTheBike17 <mchaellehman123@gmail.com>
2026-03-02 16:58:58 +0200

    Update write-env.js

--- write-env.js
+++ write-env.js
@@ -1,7 +1,7 @@
 const { writeFileSync } = require('fs');
 
-const url = process.env.SUPABASE_URL || 'https://vqqcgknwobmssgfuvfyq.supabase.com';
-const key = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxcWNna253b2Jtc3NnZnV2ZnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNTcyMTAsImV4cCI6MjA4NjkzMzIxMH0.Ml5aPoGw5lEasxPfVLmxB0gK5mapS0UUuhQUMjmPk3E';
+const url = process.env.SUPABASE_URL || '';
+const key = process.env.SUPABASE_ANON_KEY || '';
 
 const output = `window.env = { SUPABASE_URL: '${url}', SUPABASE_ANON_KEY: '${key}' };`;
 writeFileSync('js/env.js', output);
